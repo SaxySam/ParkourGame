09ea12afccceaba698d82aa9e9cac3adbabfc952
Samuel Kennedy <107587494+SaxySam@users.noreply.github.com>
2025-10-14 18:07:05 +1300

    Tweaks - Player destroys themself on this branch for some reason: must investigate!

--- Assets/Scripts/DrawGizmo.cs
+++ Assets/Scripts/DrawGizmo.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 namespace Ske131
 {
@@ -10,25 +13,48 @@ namespace Ske131
             Ray,
             Line,
             Cube,
-            Wirecube,
+            WireCube,
             Sphere,
-            Wiresphere,
+            WireSphere,
+        }
+
+        public enum RayDirection
+        {
+            Up,
+            Down,
+            Left,
+            Right,
+            Forward,
+            Back
         }
 
         public GizmoShape gizmoShape;
+        [EnumButtons] public RayDirection rayDirection;
         public Color gizmoColor = Color.white;
         public Vector3 gizmoOffset = Vector3.zero;
         public float gizmoScale = 1f;
         public bool matchObjectLocalScale;
+        private Vector3 _direction = Vector3.zero;
 
         void OnDrawGizmos()
         {
             Gizmos.color = gizmoColor;
 
+            _direction = rayDirection switch
+            {
+                RayDirection.Up => Vector3.up,
+                RayDirection.Down => Vector3.down,
+                RayDirection.Left => Vector3.left,
+                RayDirection.Right => Vector3.right,
+                RayDirection.Forward => Vector3.forward,
+                RayDirection.Back => Vector3.back,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
             switch (gizmoShape)
             {
                 case GizmoShape.Ray:
-                    Gizmos.DrawRay(transform.position + gizmoOffset, matchObjectLocalScale ? transform.forward * transform.localScale.z : transform.forward * gizmoScale);
+                    Gizmos.DrawRay(transform.position + gizmoOffset, matchObjectLocalScale ? _direction * transform.localScale.z : _direction * gizmoScale);
                     break;
                 case GizmoShape.Line:
                     Gizmos.DrawLine(transform.position + gizmoOffset, new Vector3(0, 0, 0));
@@ -36,18 +62,17 @@ namespace Ske131
                 case GizmoShape.Cube:
                     Gizmos.DrawCube(transform.position + gizmoOffset, matchObjectLocalScale ? transform.localScale : new Vector3(gizmoScale, gizmoScale, gizmoScale));
                     break;
-                case GizmoShape.Wirecube:
+                case GizmoShape.WireCube:
                     Gizmos.DrawWireCube(transform.position + gizmoOffset, matchObjectLocalScale ? transform.localScale : new Vector3(gizmoScale, gizmoScale, gizmoScale));
                     break;
                 case GizmoShape.Sphere:
                     Gizmos.DrawSphere(transform.position + gizmoOffset, matchObjectLocalScale ? transform.localScale.y : gizmoScale);
                     break;
-                case GizmoShape.Wiresphere:
+                case GizmoShape.WireSphere:
                     Gizmos.DrawWireSphere(transform.position + gizmoOffset, matchObjectLocalScale ? transform.localScale.y : gizmoScale);
                     break;
                 default:
-                    break;
-
+                    throw new ArgumentOutOfRangeException();
             }
         }
     }
