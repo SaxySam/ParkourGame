cb5c78bf4de126127db7d570e62f534df049aec4
Samuel Kennedy <107587494+SaxySam@users.noreply.github.com>
2025-11-06 16:37:41 +1300

    Setting up for particle systems - Toon Shader import and helpful links

---
+++ Assets/ParticleSystemns/Links.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Links : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // https://www.youtube.com/watch?v=FkJ0uEsK8Sk - Odyssey particles
+        // https://www.youtube.com/shorts/Qu6ANC3FHk4
+        // https://discussions.unity.com/t/puff-of-dust-effects/816268
+        // https://www.youtube.com/watch?v=NiwsmkR81Ho
+        
+        // https://www.youtube.com/watch?v=UWjpkhShB28 - Speed Lines
+        // https://www.youtube.com/watch?v=94axoRmBtqQ
+        
+    }
+}
